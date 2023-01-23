706ecb86a7b5c387e40cde3ae1800fe7933eacc5
jascolil01 <jayasco246@gmail.com>
2023-01-23 15:08:15 -0500

    added values to KJQA, need to find a way to add values up

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let suits = [`H`, `C`, `S`, `D`]
-let cards = [`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `J`, `Q`, `K`, `A`]
+let cards = [
+  `2`,
+  `3`,
+  `4`,
+  `5`,
+  `6`,
+  `7`,
+  `8`,
+  `9`,
+  `10`,
+  `J=10`,
+  `Q=10`,
+  `K=10`,
+  `A=11`
+]
 let deck = []
 let btnPlayer = document.getElementById(`player`)
 let btnReset = document.getElementById(`reset`)
@@ -10,7 +24,7 @@ suits.forEach((x) => {
   })
 })
 deck.sort((a, b) => 0.5 - Math.random())
-
+console.log(deck)
 let compareChoice = () => {
   // if (value < 21) {
   //   btnPlayer.addEventListener(`click`, () => {
@@ -72,13 +86,26 @@ btnReset.addEventListener(`click`, () => {
 })
 
 for (let i = 0; i < testP.hand.length; i++) {
-  const replaced = testP.hand[i].replace(/\D/g, '') // 👉️ '123'
+  const replaced = testP.hand[i].replace(/\D/g, '')
   console.log(replaced)
 
   let num
-
+  let z = []
   if (replaced !== '') {
-    num = Number(replaced) // 👉️ 123
+    num = Number(replaced)
+    z.push(num)
   }
-  console.log(num)
+  console.log(z)
+  // let results = z.reduce((x, y) => {
+  //   return x + y
+  // }, 0)
+
+  // console.log(results)
+  // if (results < 21) {
+  //   console.log(`hit or stand`)
+  // } else if (results === 21) {
+  //   console.log(`BlackJack`)
+  // } else {
+  //   console.log(`try again`)
+  // }
 }
